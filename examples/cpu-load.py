cd7284b7c7cba9ec32911dd0248bcd41894b6e4d
John Blackmore <blackmorejd@googlemail.com>
2014-11-16 20:40:56 +0000

    Added cpu-load example

---
+++ examples/cpu-load.py
@@ -0,0 +1,92 @@
+# Copyright (c) 2014 Adafruit Industries
+# Author: Tony DiCola
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+import time
+import os
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+
+import Image
+import ImageDraw
+import ImageFont
+
+
+# Note you can change the I2C address by passing an i2c_address parameter like:
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=24, i2c_address=0x3C)
+
+# Initialize library.
+disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = 2
+shape_width = 20
+top = padding
+bottom = height-padding
+
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+draw.line((2, 12, 125, 12), fill=255)
+uname = os.popen('uname -n').readline().replace('\n', '')
+ip_addr = os.popen('ip addr show wlan0 | grep inet').readline().split()[1].split('/')[0]
+
+# Load default font.
+font = ImageFont.truetype('pixelmix.ttf', 8)
+draw.text((2,2), uname, font=font, fill=255)
+draw.text((2,34), "IP: %s" % (ip_addr), font=font, fill=255)
+
+while True:
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0,13,width,33), outline=0, fill=0)
+
+    # CPU temp
+    res = os.popen('cat /sys/class/thermal/thermal_zone0/temp').readline()
+    res = res.replace('\n', '')
+    draw.text((2,14), "CPU temp: %0.1fc" % (float(res)/1000), font=font, fill=255)
+
+    # Load Average
+    res = os.popen('uptime').readline()
+    res = res.replace('\n', '')
+    pos = res.index('age:')
+    load = res[pos+5:pos+9]
+    draw.text((2,24), "Load Average: %s" % (load), font=font, fill=255)
+
+    # Write two lines of text.
+#    draw.text((padding, top),    'Hello',  font=font, fill=255)
+#    draw.text((padding, top+20), 'World!', font=font, fill=255)
+
+    # Display image.
+    disp.image(image)
+    disp.display()
+
+    time.sleep(2)
--- examples/image.py
+++ examples/image.py
@@ -41,10 +41,10 @@ SPI_DEVICE = 0
 # SPI_DEVICE = 0
 
 # 128x32 display with hardware I2C:
-disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 
 # 128x64 display with hardware I2C:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
 
 # 128x32 display with hardware SPI:
 # disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
